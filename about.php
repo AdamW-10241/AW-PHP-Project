847bfe0d5fefacca071fa487c07fcb8ec7acfb3a
Adam Williams <158815319+AdamW-10241@users.noreply.github.com>
2025-02-20 00:46:54 +0000

    Added Navbar Links

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'vendor/autoload.php';
+// Classes used in this stage
+use Adam\AwPhpProject\App;
+// Create app from App class
+$app = new App();
+$site_name = $app -> site_name;
+// Loading the twig template
+$loader = new \Twig\Loader\FilesystemLoader( 'templates' );
+$twig = new \Twig\Environment( $loader );
+$template = $twig -> load( 'about.twig' );
+// Render the output
+echo $template -> render( [
+    'site_name' => $site_name
+] );
+?>
+
