bb9e4cce9109fec26faab5e62682556d046ed5bf
Adam Williams <158815319+AdamW-10241@users.noreply.github.com>
2025-03-30 08:22:54 +0000

    Added More BoardGames

--- database.sql
+++ database.sql
@@ -175,7 +175,18 @@ SELECT * FROM (
     SELECT 'Fantasy Flight Games' UNION ALL
     SELECT 'Stronghold Games' UNION ALL
     SELECT 'Rio Grande Games' UNION ALL
-    SELECT 'Pandasaurus Games'
+    SELECT 'Pandasaurus Games' UNION ALL
+    SELECT 'Next Move Games' UNION ALL
+    SELECT 'Dire Wolf' UNION ALL
+    SELECT 'Capstone Games' UNION ALL
+    SELECT 'Maestro Media' UNION ALL
+    SELECT 'Lookout Games' UNION ALL
+    SELECT 'Schmidt Spiele' UNION ALL
+    SELECT 'Flatout Games' UNION ALL
+    SELECT 'Gamewright' UNION ALL
+    SELECT 'Studio71' UNION ALL
+    SELECT 'AdMagic Games' UNION ALL
+    SELECT 'Ludically'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM Publisher);
 
@@ -191,7 +202,17 @@ SELECT * FROM (
     SELECT 'Corey', 'Konieczka' UNION ALL
     SELECT 'Matt', 'Leacock' UNION ALL
     SELECT 'Jacob', 'Fryxelius' UNION ALL
-    SELECT 'Klaus', 'Teuber'
+    SELECT 'Klaus', 'Teuber' UNION ALL
+    SELECT 'Michael', 'Kiesling' UNION ALL
+    SELECT 'Paul', 'Dennen' UNION ALL
+    SELECT 'Dennis', 'Chan' UNION ALL
+    SELECT 'Helge', 'Ostertag' UNION ALL
+    SELECT 'Edmund', 'McMillen' UNION ALL
+    SELECT 'Uwe', 'Rosenberg' UNION ALL
+    SELECT 'Kevin', 'Russ' UNION ALL
+    SELECT 'Phil', 'Walker-Harding' UNION ALL
+    SELECT 'Elan', 'Lee' UNION ALL
+    SELECT 'Christophe', 'Boelinger'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM Designer);
 
@@ -207,7 +228,18 @@ SELECT * FROM (
     SELECT 'Kwanchai', 'Moria' UNION ALL
     SELECT 'David', 'Aguilera' UNION ALL
     SELECT 'Jakub', 'Rozalski' UNION ALL
-    SELECT 'Andrew', 'Navaro'
+    SELECT 'Andrew', 'Navaro' UNION ALL
+    SELECT 'Philippe', 'Guerin' UNION ALL
+    SELECT 'Clay', 'Brooks' UNION ALL
+    SELECT 'Franz', 'Vohwinkel' UNION ALL
+    SELECT 'Lukas', 'Siegmon' UNION ALL
+    SELECT 'Krystal', 'Fleming' UNION ALL
+    SELECT 'Carrie', 'Cantwell' UNION ALL
+    SELECT 'Leon', 'Schiffer' UNION ALL
+    SELECT 'Beth', 'Sobel' UNION ALL
+    SELECT 'Nan', 'Rangsima' UNION ALL
+    SELECT 'Matthew', 'Inman' UNION ALL
+    SELECT 'Vincent', 'Boulanger'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM Artist);
 
@@ -225,7 +257,19 @@ SELECT * FROM (
     SELECT 'SETI: Search for Extraterrestrial Intelligence', 'A scientific exploration game', 2023, 'Players work as SETI researchers, analyzing signals from space and making groundbreaking discoveries. Balance your research between different types of signals and manage your limited resources.', '1-4', '12+', '45-60', 'board_9.png', 'cooperative,science,resource-management' UNION ALL
     SELECT 'Terraforming Mars', 'Transform the Red Planet', 2016, 'In Terraforming Mars, players take on the role of corporations working together to make Mars habitable. Play project cards, build a production engine, and compete for milestones and awards.', '1-5', '12+', '120', 'board_10.png', 'strategy,engine-building,science' UNION ALL
     SELECT 'Monkey Palace', 'A primate management game', 2023, 'Run your own primate sanctuary! Care for different species of monkeys, manage their habitats, and ensure their well-being while attracting visitors to your sanctuary.', '2-4', '10+', '45-60', 'board_11.png', 'management,animals,economic' UNION ALL
-    SELECT 'Deep Rock Galactic: The Board Game', 'A cooperative mining adventure', 2023, 'Based on the popular video game, players work together as space dwarves mining valuable resources on a hostile alien planet. Fight off creatures, gather resources, and complete missions.', '1-4', '12+', '60-90', 'board_12.png', 'cooperative,adventure,combat'
+    SELECT 'Deep Rock Galactic: The Board Game', 'A cooperative mining adventure', 2023, 'Based on the popular video game, players work together as space dwarves mining valuable resources on a hostile alien planet. Fight off creatures, gather resources, and complete missions.', '1-4', '12+', '60-90', 'board_12.png', 'cooperative,adventure,combat' UNION ALL
+    SELECT 'Azul', 'Artfully embellish the walls of your palace by drafting the most beautiful tiles.', 2017, 'In the game Azul, players take turns drafting colored tiles from suppliers to their player board. Later in the round, players score points based on how they placed their tiles to decorate the palace.', '2-4', '8+', '30-45', 'board_13.png', 'tile-laying,abstract,strategy' UNION ALL
+    SELECT 'Clank!: Catacombs', 'Deck-building adventure meets tile-laying in the newest incarnation of Clank!', 2022, 'Prisoners are counting on you to free them. Ghosts, once disturbed, may haunt you to death. Despite all that, it is time to leave the board behind with Clank! Catacombs, a standalone deck-building adventure.', '2-4', '13+', '45-90', 'board_14.png', 'strategy,cooperative,adventure' UNION ALL
+    SELECT 'Beyond the Sun', 'Collectively develop a tech tree to fuel new discoveries and colonize space.', 2020, 'Beyond the Sun is a space civilization game in which players collectively decide the technological progress of humankind at the dawn of the Spacefaring Era.', '2-4', '14+', '60-120', 'board_15.png', 'strategy,space,management' UNION ALL
+    SELECT 'Age of Innovation', 'Terraform the world to expand your faction and create innovations on the way.', 2023, 'Twelve factions, each with unique characteristics, populate this world of varying terrains. Here you will compete to erect buildings and merge them into cities.', '1-5', '14+', '40-200', 'board_16.png', 'strategy,economic,management' UNION ALL
+    SELECT 'The Binding of Isaac: Four Souls', 'The Binding of Isaac multiplayer card game.', 2018, 'Experience the haunted and harrowing world of The Binding of Isaac: Four Souls yourself in this faithful adaptation. Collect treasure, gather loot, defeat monsters, and be the first to collect four souls.', '2-4', '13+', '30-60', 'board_17.png', 'push-your-luck,dice-rolling,card-game' UNION ALL
+    SELECT 'Patchwork', 'Piece together a quilt and leave no holes to become the button master.', 2014, 'Two players compete to build the most aesthetic (and high-scoring) patchwork quilt on a personal 9x9 game board.', '2', '8+', '15-30', 'board_18.png', 'abstract,economic,environmental' UNION ALL
+    SELECT 'That''s Pretty Clever!', 'Draft dice to mark numbers on your scoresheet, comboing bonuses to power your points.', 2018, 'Choose your dice cleverly to enter them into the matching colored areas on your score sheet, putting together tricky chain-scoring opportunities, and racking up the points!', '1-4', '8+', '30', 'board_19.png', 'push-your-luck,dice-rolling,management' UNION ALL
+    SELECT 'Calico', 'Sew a quilt, collect buttons, attract cats!', 2020, 'In Calico, players compete to sew the coziest quilt as they collect and place patches of different colors and patterns. Make a pattern and attract the cuddliest cats!.', '1-4', '10+', '30-45', 'board_20.png', 'abstract,animals,family' UNION ALL
+    SELECT 'Sushi Go Party!', 'Pass sushi around a bigger table and take the best dishes.', 2016, 'Customize each game by choosing à la carte from a menu of more than twenty delectable dishes.', '2-8', '8+', '20', 'board_21.png', 'family,card-game,strategy' UNION ALL
+    SELECT 'Tapeworm', 'Race to be the first to empty your hand by creating & cutting up tapeworms.', 2020, 'In Tapeworm, players race to be the first to get rid of all of their cards by connecting and growing the wriggling masses of different colored worm bodies!', '2-4', '8+', '15-30', 'board_22.png', 'family,management,card-game' UNION ALL
+    SELECT 'Exploding Kittens', 'Ask for favors, attack friends, see the future- whatever it takes to avoid exploding!', 2015, 'Exploding Kittens is a kitty-powered version of Russian Roulette. Players take turns drawing cards until someone draws an exploding kitten and loses the game!', '2-5', '7+', '15', 'board_23.png', 'animals,management,card-game' UNION ALL
+    SELECT 'Archipelago', 'Settlers work together and compete for resources to survive the new world.', 2012, 'As Renaissance European powers competing in the exploration of a Pacific or Caribbean archipelago, players will explore territories, harvest resources and betray!', '2-5', '14+', '30-240', 'board_24.png', 'strategy,economic,exploration'
 ) AS tmp
 WHERE NOT EXISTS (SELECT 1 FROM BoardGame);
 
@@ -243,7 +287,19 @@ VALUES
     (9, 9), -- SETI - Rio Grande Games
     (10, 10), -- Terraforming Mars - Pandasaurus Games
     (11, 1), -- Monkey Palace - Roxley Games
-    (12, 5); -- Deep Rock Galactic - CMON Limited
+    (12, 5), -- Deep Rock Galactic - CMON Limited
+    (13, 11), -- Azul - Next Move Games
+    (14, 12), -- Clank!: Catacombs - Dire Wolf
+    (15, 9), -- Beyond the Sun - Rio Grande Games
+    (16, 13), -- Age of Innovation - Capstone Games
+    (17, 14), -- The Binding of Isaac: Four Souls - Maestro Media
+    (18, 15), -- Patchwork - Lookout Games
+    (19, 16), -- That's Pretty Clever! - Schmidt Spiele
+    (20, 17), -- Calico - Flatout Games
+    (21, 18), -- Sushi Go Party! - Gamewright
+    (22, 19), -- Tapeworm - Studio71
+    (23, 20), -- Exploding Kittens - AdMagic Games
+    (24, 21); -- Archipelago - Ludically
 
 -- Link board games with designers only if no links exist
 INSERT INTO BoardGame_Designer (boardgame_id, designer_id)
@@ -259,7 +315,19 @@ VALUES
     (9, 9), -- SETI - Jacob Fryxelius
     (10, 10), -- Terraforming Mars - Klaus Teuber
     (11, 1), -- Monkey Palace - Martin Wallace
-    (12, 2); -- Deep Rock Galactic - Alexander Pfister
+    (12, 2), -- Deep Rock Galactic - Alexander Pfister
+    (13, 11), -- Azul - Michael Kiesling
+    (14, 12), -- Clank!: Catacombs - Paul Dennen
+    (15, 13), -- Beyond the Sun - Dennis Chan
+    (16, 14), -- Age of Innovation - Helge Ostertag
+    (17, 15), -- The Binding of Isaac: Four Souls - Edmund McMillen
+    (18, 16), -- Patchwork - Uwe Rosenberg
+    (19, 3), -- That's Pretty Clever! - Wolfgang Warsch
+    (20, 17), -- Calico - Kevin Russ
+    (21, 18), -- Sushi Go Party! - Phil Walker-Harding
+    (22, 15), -- Tapeworm - Edmund McMillen
+    (23, 19), -- Exploding Kittens - Elan Lee
+    (24, 20); -- Archipelago - Christophe Boelinger
 
 -- Link board games with artists only if no links exist
 INSERT INTO BoardGame_Artist (boardgame_id, artist_id)
@@ -275,7 +343,20 @@ VALUES
     (9, 9), -- SETI - Jakub Rozalski
     (10, 10), -- Terraforming Mars - Andrew Navaro
     (11, 1), -- Monkey Palace - Lina Cossette
-    (12, 2); -- Deep Rock Galactic - Vincent Dutrait
+    (12, 2), -- Deep Rock Galactic - Vincent Dutrait
+    (13, 6), -- Azul - Chris Quilliams
+    (13, 11), -- Azul - Philippe Guerin
+    (14, 12), -- Clank!: Catacombs - Clay Brooks
+    (15, 13), -- Beyond the Sun - Franz Vohwinkel
+    (16, 14), -- Age of Innovation - Lukas Siegmon
+    (17, 15), -- The Binding of Isaac: Four Souls - Krystal Fleming
+    (18, 16), -- Patchwork - Carrie Cantwell
+    (19, 17), -- That's Pretty Clever! - Leon Schiffer
+    (20, 18), -- Calico - Beth Sobel
+    (21, 19), -- Sushi Go Party! - Nan Rangsima
+    (22, 15), -- Tapeworm - Krystal Fleming
+    (23, 20), -- Exploding Kittens - Matthew Inman
+    (24, 21); -- Archipelago - Vincent Boulanger
 
 -- Create news table
 CREATE TABLE IF NOT EXISTS news (
